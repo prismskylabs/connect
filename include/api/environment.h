2f0eb665cf267e467550933049725d91b1398641
Huu Nguyen <whoshuu@gmail.com>
2016-10-11 13:47:05 -0700

    Add environment.h header for environment constants

---
+++ include/api/environment.h
@@ -0,0 +1,19 @@
+#ifndef PRISM_CONNECT_API_Environment_H_
+#define PRISM_CONNECT_API_Environment_H_
+
+#include <iostream>
+
+namespace prism {
+namespace connect {
+namespace api {
+namespace environment {
+
+static const std::string ServerRoot = API_ROOT;
+static const std::string AuthToken = API_TOKEN;
+
+} // naespace environment
+} // namespace api
+} // namespace connect
+} // namespace prism
+
+#endif // PRISM_CONNECT_API_Environment_H_
