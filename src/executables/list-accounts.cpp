5f8df94993e8d193afd41b7705e3cf88342a9fb3
Huu Nguyen <whoshuu@gmail.com>
2016-10-11 16:08:49 -0700

    Add example list-accounts executable

---
+++ src/executables/list-accounts.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include "api/client.h"
+#include "api/environment.h"
+
+using namespace prism::connect::api;
+
+int main() {
+    Client client{environment::ApiRoot, environment::ApiToken};
+    
+    auto accounts = client.QueryAccounts();
+
+    for (const auto& account : accounts) {
+        std::cout << "Account[" << account.id_ << "]:" << std::endl;
+        std::cout << "\tName: " << account.name_ << std::endl;
+        std::cout << "\tUrl: " << account.url_ << std::endl;
+        std::cout << "\tInstruments Url: " << account.instruments_url_ << std::endl;
+        std::cout << std::endl;
+    }
+}
