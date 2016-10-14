88951152c7d21a786f4a8535eba10a42d2cb7fdd
Huu Nguyen <whoshuu@gmail.com>
2016-10-14 12:26:12 -0700

    Add string util function

---
+++ include/util/string.h
@@ -0,0 +1,16 @@
+#ifndef PRISM_CONNECT_UTIL_String_H_
+#define PRISM_CONNECT_UTIL_String_H_
+
+#include <string>
+
+namespace prism {
+namespace connect {
+namespace util {
+
+std::string ParseMimeType(const std::string& filename);
+
+} // namespace util
+} // namespace connect
+} // namespace prism
+
+#endif // PRISM_CONNECT_UTIL_String_H_
---
+++ src/util/string.cpp
@@ -0,0 +1,34 @@
+#include "util/string.h"
+
+#include <string>
+#include <unordered_map>
+
+namespace prism {
+namespace connect {
+namespace util {
+
+static const std::unordered_map<std::string, std::string> mime_mapping{{"png", "image/png"},
+                                                                       {"jpg", "image/jpeg"},
+                                                                       {"jpeg", "image/jpeg"},
+                                                                       {"mp4", "video/mp4"},
+                                                                       {"h264", "video/h264"}};
+
+std::string ParseMimeType(const std::string& filename) {
+    auto extension_character = filename.find_last_of('.');
+
+    if (extension_character == std::string::npos) {
+        return "";
+    }
+
+    auto lookup = mime_mapping.find(filename.substr(extension_character + 1));
+
+    if (lookup == mime_mapping.end()) {
+        return "";
+    }
+
+    return lookup->second;
+}
+
+} // namespace util
+} // namespace connect
+} // namespace prism
