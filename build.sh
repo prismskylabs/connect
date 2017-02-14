789d52a89c33ccdf307aef6d410ff3b78149edd3
Ivan Onyshchenko <ivan.onyshchenko@prism.com>
2017-02-14 16:11:19 +0200

    EDGE-178 Make build log on CI server show commands

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ BUILD_DIR="./build"
 BUILD_TYPE=Release
 NJOBS="$(getconf _NPROCESSORS_ONLN)"
 MAKE_DELIVERY=0
+PRC_CMAKE_EXTRA_FLAGS=""
 
 parse_cmd_line(){
     for i in "$@"
@@ -21,6 +22,8 @@ parse_cmd_line(){
         BUILD_TYPE="${i#*=}";;
         --delivery)
         MAKE_DELIVERY=1;;
+        --debug)
+        PRC_CMAKE_EXTRA_FLAGS="-DCMAKE_RULE_MESSAGES:BOOL=OFF -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON";;
         *)
         ;;
     esac
@@ -32,7 +35,7 @@ parse_cmd_line $@
 # generate make file
 # uncomment to show command line when building
 # cmake -DCMAKE_RULE_MESSAGES:BOOL=OFF -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -B$BUILD_DIR -H. "-DCMAKE_BUILD_TYPE=${BUILD_TYPE}"
-cmake -B$BUILD_DIR -H. "-DCMAKE_BUILD_TYPE=${BUILD_TYPE}"
+cmake $PRC_CMAKE_EXTRA_FLAGS -B$BUILD_DIR -H. "-DCMAKE_BUILD_TYPE=${BUILD_TYPE}"
 
 # build
 cmake --build $BUILD_DIR -- -j$NJOBS
