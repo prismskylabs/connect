6c99dd2e9035e9ad8fc99f654c8130f6ed5ad586
Ivan Onyshchenko <ionyshch-prismsl@users.noreply.github.com>
2017-02-14 17:10:37 +0200

    Feature/edge 178 (#24)
    * EDGE-178 Make build log on CI server show commands
    * Disabled Entering/Leaving directory in verbose cmake output.
    * Fixed getting tag associated with HEAD

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ BUILD_DIR="./build"
 BUILD_TYPE=Release
 NJOBS="$(getconf _NPROCESSORS_ONLN)"
 MAKE_DELIVERY=0
+PRC_CMAKE_EXTRA_FLAGS=""
 
 parse_cmd_line(){
     for i in "$@"
@@ -21,6 +22,8 @@ parse_cmd_line(){
         BUILD_TYPE="${i#*=}";;
         --delivery)
         MAKE_DELIVERY=1;;
+        --debug)
+        PRC_CMAKE_EXTRA_FLAGS="-DCMAKE_RULE_MESSAGES:BOOL=OFF -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON";;
         *)
         ;;
     esac
@@ -32,16 +35,16 @@ parse_cmd_line $@
 # generate make file
 # uncomment to show command line when building
 # cmake -DCMAKE_RULE_MESSAGES:BOOL=OFF -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -B$BUILD_DIR -H. "-DCMAKE_BUILD_TYPE=${BUILD_TYPE}"
-cmake -B$BUILD_DIR -H. "-DCMAKE_BUILD_TYPE=${BUILD_TYPE}"
+cmake $PRC_CMAKE_EXTRA_FLAGS -B$BUILD_DIR -H. "-DCMAKE_BUILD_TYPE=${BUILD_TYPE}"
 
 # build
-cmake --build $BUILD_DIR -- -j$NJOBS
+cmake --build $BUILD_DIR -- -j$NJOBS --no-print-directory
 
 if [ -z ${TEAMCITY_VERSION+x} ]; then
     # no-op
     :
 else
-    TAG=`git describe --exact-match HEAD 2>/dev/null || :`
+    TAG=$(git describe --exact-match HEAD 2>/dev/null || :)
 
     if [ "${TAG}" ]; then
         MAKE_DELIVERY=1
@@ -52,5 +55,5 @@ echo "Make delivery: ${MAKE_DELIVERY}"
 
 # archive package
 if (( ${MAKE_DELIVERY} != 0 )); then
-    cmake --build $BUILD_DIR --target delivery;
+    cmake --build $BUILD_DIR --target delivery -- --no-print-directory
 fi
