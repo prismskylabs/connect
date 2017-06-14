4e10f4cf87f1eef0e040c07f542a80aabc3661ff
Ivan Onyshchenko <ionyshch-prismsl@users.noreply.github.com>
2017-06-14 13:25:19 +0300

    Set version to 1.8.0 (#64)

--- version.cmake
+++ version.cmake
@@ -1,4 +1,4 @@
 # Copyright (C) 2017 Prism Skylabs
 set(ConnectSDK_VERSION_MAJOR 1)
-set(ConnectSDK_VERSION_MINOR 7)
-set(ConnectSDK_VERSION_REVISION 1)
+set(ConnectSDK_VERSION_MINOR 8)
+set(ConnectSDK_VERSION_REVISION 0)
