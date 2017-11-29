bea842008bfe5d316e1df3e1a234167ab6c9ae37
Yuriy Solovyov <yuriy.solovyov@prism.com>
2017-11-29 10:04:29 +0200

    Switch to Xenial. Use appropriate artifacts. (#83)

--- deps.cmake
+++ deps.cmake
@@ -1,4 +1,4 @@
 # Copyright (C) 2017 Prism Skylabs
 set(ARTIFACTORY_URL "https://artifactory.dev.prismsl.net/artifactory")
-set(OPENCV_VERSION "3.2.0")
+set(OPENCV_VERSION "3.3.0")
 set(BOOST_VERSION  "1.62.0")
