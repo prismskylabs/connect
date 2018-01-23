6d4304ed9b086e5fc1c65725af8f570c973ea2da
Ivan Onyshchenko <ivan.onyshchenko@prism.com>
2018-01-23 16:50:50 +0200

    Set version to 1.13.0

--- version.cmake
+++ version.cmake
@@ -1,4 +1,4 @@
-# Copyright (C) 2017 Prism Skylabs
+# Copyright (C) 2017-2018 Prism Skylabs
 set(ConnectSDK_VERSION_MAJOR 1)
-set(ConnectSDK_VERSION_MINOR 12)
-set(ConnectSDK_VERSION_REVISION 3)
+set(ConnectSDK_VERSION_MINOR 13)
+set(ConnectSDK_VERSION_REVISION 0)
